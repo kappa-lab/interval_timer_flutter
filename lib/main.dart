0ebab2b440e2af1b0b086b65d800349b4f4eb563
yamagami kenichi <gamiken@kappa-lab.com>
2022-01-28 12:07:09 +0900

    kIsWeb

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:interval_timer/workout.dart';
 import 'package:wakelock/wakelock.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Wakelock.enable();
+    if (!kIsWeb) Wakelock.enable();
 
     return MaterialApp(
       title: 'INTERVAL TIMER',
@@ -42,9 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
