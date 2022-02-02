f9e55898545d449a2867db35c40f520d261b61cc
yamagami kenichi <gamiken@kappa-lab.com>
2022-02-02 16:38:26 +0900

    style

--- lib/indicator.dart
+++ lib/indicator.dart
@@ -9,13 +9,18 @@ class Indicator extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    const shadowRect = Rect.fromLTWH(-103, -3, 206, 206);
     const rect = Rect.fromLTWH(-100, 0, 200, 200);
     const innerRect = Rect.fromLTWH(-100 + 5, 5, 200 - 10, 200 - 10);
-    final bg = Paint()..color = Colors.black38;
-    final innerPt = Paint()..color = Colors.white;
-    final fill = Paint()..color = Colors.black87;
+    final shadow = Paint()
+      ..color = const Color.fromARGB(0xFF, 0x18, 0x18, 0x18);
+    final bg = Paint()..color = const Color.fromARGB(0xFF, 0x0F, 0x0F, 0x0F);
+    final innerPt = Paint()
+      ..color = const Color.fromARGB(0xFF, 0x1E, 0x1E, 0x1E);
+    final fill = Paint()..color = const Color.fromARGB(0xFF, 0xE0, 0x30, 0x13);
 
     canvas
+      ..drawArc(shadowRect, 0, 7, true, shadow)
       ..drawArc(rect, 0, 7, true, bg)
       ..drawArc(rect, 4.7, _progress * 6.28319, true, fill)
       ..drawArc(innerRect, 0, 7, false, innerPt);
--- lib/main.dart
+++ lib/main.dart
@@ -36,20 +36,28 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class _ViewModel {
+  String remainTime;
+  double progress = 0.0;
+  double nextProgress = 0.0;
+  _ViewModel(this.remainTime);
+}
+
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  bool running = false;
-  WorkOut data = WorkOut(const Duration(seconds: 5), 1);
+  bool _running = false;
+  WorkOut _data = WorkOut(const Duration(seconds: 5), 1);
   late WorkOutTimer timer;
-  late String remainTime = data.time.inSeconds.toString();
-  double progress = 0;
-  double newProgress = 0.0;
+  late _ViewModel _viewModel = _ViewModel(_data.time.inSeconds.toString());
 
   late final AnimationController percentageAnimationController =
       AnimationController(vsync: this, duration: const Duration(seconds: 1))
         ..addListener(() {
           setState(() {
-            progress = lerpDouble(
-                progress, newProgress, percentageAnimationController.value)!;
+            _viewModel.progress = lerpDouble(
+              _viewModel.progress,
+              _viewModel.nextProgress,
+              percentageAnimationController.value,
+            )!;
           });
         });
 
@@ -58,73 +66,82 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(0xFF, 0x1E, 0x1E, 0x1E),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: Column(
-              children: [
-                const Padding(padding: EdgeInsets.all(20.0)),
-                CustomPaint(painter: Indicator(progress)),
-                const Padding(padding: EdgeInsets.all(36.0)),
-                Text(
-                  remainTime,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 48.0,
-                      color: Color(0xFF000000),
-                      fontFamily: "RobotoMono"),
-                ),
-                const Padding(padding: EdgeInsets.all(60.0)),
-                ElevatedButton(
-                  onPressed: _onStartPressed,
-                  child: Text(running ? "STOP" : "START"),
-                ),
-              ],
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.all(20.0)),
+                  CustomPaint(painter: Indicator(_viewModel.progress)),
+                  const Padding(padding: EdgeInsets.all(36.0)),
+                  Text(
+                    _viewModel.remainTime,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 48.0,
+                        color: Color(0xFFCCCCCC),
+                        fontFamily: "RobotoMono"),
+                  ),
+                  const Padding(padding: EdgeInsets.all(60.0)),
+                  ElevatedButton(
+                    onPressed: _onStartPressed,
+                    child: Text(_running ? "STOP" : "START"),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Padding(padding: EdgeInsets.all(15.0)),
-          Text(
-            "[debug] $debugMsg",
-            style: const TextStyle(
-                fontSize: 12.0,
-                color: Color(0xFF000000),
-                fontFamily: "RobotoMono"),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'time'),
-            onChanged: _onTimeChanged,
-            style: const TextStyle(
-                color: Color(0xFF000000),
-                fontWeight: FontWeight.w200,
-                fontFamily: "Roboto"),
-          ),
-        ],
-      ),
+            const Padding(padding: EdgeInsets.all(15.0)),
+            Container(
+                color: const Color(0xFFE3E3E3),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "[debug] $debugMsg",
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          color: Color(0xFF000000),
+                          fontFamily: "RobotoMono",
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'time'),
+                        onChanged: _onTimeChanged,
+                        style: const TextStyle(
+                            color: Color(0xFF000000),
+                            fontWeight: FontWeight.w200,
+                            fontFamily: "Roboto"),
+                      ),
+                    ])),
+          ]),
     );
   }
 
   void _onTimeChanged(String? input) {
     setState(() {
       if (input == null) return;
-      data = WorkOut(Duration(seconds: int.parse(input)), 1);
-      remainTime = "${data.time.inSeconds}";
-      debugMsg = "set: ${data.time.inSeconds}";
+      _data = WorkOut(Duration(seconds: int.parse(input)), 1);
+      _viewModel.remainTime = "${_data.time.inSeconds}";
+      debugMsg = "set: ${_data.time.inSeconds}";
     });
   }
 
   void _onStartPressed() {
     setState(() {
-      running = !running;
-      if (running) {
-        timer = WorkOutTimer(data.time, _onComplete, _onTick);
+      _running = !_running;
+      if (_running) {
+        _viewModel = _ViewModel(_data.time.inSeconds.toString())
+          ..progress = 0.005;
+        timer = WorkOutTimer(_data.time, _onComplete, _onTick);
         timer.start();
-        remainTime = "${data.time.inSeconds}";
-        progress = 0.005;
 
-        debugMsg = "start: ${data.time.inSeconds}";
+        debugMsg = "start: ${_data.time.inSeconds}";
       } else {
         timer.stop();
       }
@@ -133,9 +150,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   void _onComplete() {
     setState(() {
-      remainTime = "0";
-      running = false;
-      newProgress = 1;
+      _running = false;
+      _viewModel.remainTime = "0";
+      _viewModel.nextProgress = 1;
       percentageAnimationController.forward(from: 0.0);
       debugMsg = "comp : 0 ";
     });
@@ -145,10 +162,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     setState(() {
       int r = (remain.inMilliseconds / 1000.0).round();
       int sec = Duration(seconds: r).inSeconds;
-      int end = data.time.inSeconds;
+      int end = _data.time.inSeconds;
 
-      newProgress = (end - sec) / end;
-      remainTime = "$sec";
+      _viewModel.nextProgress = (end - sec) / end;
+      _viewModel.remainTime = "$sec";
 
       percentageAnimationController.forward(from: 0.0);
 
