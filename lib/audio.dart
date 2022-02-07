97f7ff179d545ae5f056b3ba5803f6b9b05b8ef8
yamagami kenichi <gamiken@kappa-lab.com>
2022-02-07 14:16:32 +0900

    update se

--- lib/audio.dart
+++ lib/audio.dart
@@ -1,16 +1,23 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class SEPlayer {
-  static const String _fileD0 = 'd0.wav';
-  static const String _fileD1 = 'd1.wav';
-
   final AudioCache _cache;
 
   SEPlayer()
       : _cache = AudioCache(fixedPlayer: AudioPlayer())
-          ..loadAll([_fileD0, _fileD1]);
+          ..loadAll([
+            _FilePath.start,
+            _FilePath.tick,
+            _FilePath.comp,
+          ]);
 
-  void playStart() => _cache.play(_fileD0);
-  void playTick() => _cache.play(_fileD0);
-  void playComplete() => _cache.play(_fileD1);
+  void playStart() => _cache.play(_FilePath.start);
+  void playTick() => _cache.play(_FilePath.tick);
+  void playComplete() => _cache.play(_FilePath.comp);
+}
+
+class _FilePath {
+  static const String start = 'start.wav';
+  static const String tick = 'tick.wav';
+  static const String comp = 'comp.wav';
 }
